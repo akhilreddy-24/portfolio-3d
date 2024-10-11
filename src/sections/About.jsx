817b142de40828d23934314481da650a2f770a8e
Konreddy Akhil Reddy <113740615+akhilreddy-24@users.noreply.github.com>
2024-10-11 10:43:45 +0530

    Update About.jsx

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
   const [hasCopied, setHasCopied] = useState(false);
 
   const handleCopy = () => {
-    navigator.clipboard.writeText(' adrian@jsmastery.pro');
+    navigator.clipboard.writeText(' akhilreddykonreddy@gmail.com');
     setHasCopied(true);
 
     setTimeout(() => {
